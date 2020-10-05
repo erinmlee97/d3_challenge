c7e3f50a2c578ae4007fc11a1672d0c8fd90f186
erinmlee97 <erinmlee97@gmailcom>
2020-10-05 15:57:00 -0400

    append svg wrapper

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,6 +1,9 @@
+// Create width & height for svg
+// svg container
 var svgWidth = 960;
 var svgHeight = 500;
 
+// margin
 var margin = {
   top: 20,
   right: 40,
@@ -8,5 +11,13 @@ var margin = {
   left: 100
 };
 
+// chart area minus margins
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
+// create a svg container
+var svg = d3
+  .select("scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
